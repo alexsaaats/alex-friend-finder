10b94e16994d372745bbb32520ec67bb7f0db560
alexdecycive <alex@decycive.com>
2017-11-12 12:17:13 -0500

    First version, basic HTML routes working, content loaded, no core functions

---
+++ server.js
@@ -0,0 +1,94 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+//ACTIONS AND APP
+
+
+
+
+
+//app.get('/', (req, res) => res.send('Hello World! CONFIRM'))
+
+app.listen(3000, () => console.log('Friend finder app listening on port 3000.'));
+
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+/* 
+//TEST: 
+app.get('/', function (req, res) {
+  res.send('Hello World! CONFIRM 2')
+})
+
+*/
+
+
+// Routes
+// =============================================================
+
+//Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+/* 
+
+// Get all characters
+app.get("/all", function(req, res) {
+  res.json(characters);
+});
+
+// Search for Specific Character (or all characters) - provides JSON
+app.get("/api/:characters?", function(req, res) {
+  var chosen = req.params.characters;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < characters.length; i++) {
+      if (chosen === characters[i].routeName) {
+        return res.json(characters[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(characters);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newcharacter = req.body;
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+*/
